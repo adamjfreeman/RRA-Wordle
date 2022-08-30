2b2ba80d3c83b16912dde599a7b1111210156214
adamjfreeman <73137291+adamjfreeman@users.noreply.github.com>
2022-08-30 08:33:35 -0600

    Update script.js

--- build/script.js
+++ build/script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = THEME
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 
 console.log(rightGuessString)
 
