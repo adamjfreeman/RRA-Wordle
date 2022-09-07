cdc66fbfea7fc4d617ff8754d1a6ffc25dc81cd5
adamjfreeman <73137291+adamjfreeman@users.noreply.github.com>
2022-09-07 06:05:13 -0700

    Update script.js

--- build/script.js
+++ build/script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = WORDS[482]
+let rightGuessString = WORDS[193]
 
 console.log(rightGuessString)
 
